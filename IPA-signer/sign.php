eeb8be97a979f7fd58d9249d5f04cb205df99088
Bean55tik <156041762+Bean55tik@users.noreply.github.com>
2024-01-26 12:17:38 -0500

    Update sign.php

--- IPA-signer/sign.php
+++ IPA-signer/sign.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 $ipa = htmlspecialchars($_POST['ipa']);
 $p12 = htmlspecialchars($_POST['p12']);
 $mobileprovision = htmlspecialchars($_POST['mobileprovision']);
